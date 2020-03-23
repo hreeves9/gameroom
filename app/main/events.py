32894c4503972edde750cc6474f5ac0a72419253
Caleb Harris <calebhar@umich.edu>
2020-03-23 10:21:42 -0400

    Added play cards and display upcard functionality

--- app/main/events.py
+++ app/main/events.py
@@ -10,7 +10,7 @@ def joined(message):
     room = session.get('room')
     join_room(room)
     emit('status', {'msg': session.get('name') + ' has entered the room.'}, room=room)
- 
+
 
 @socketio.on('left', namespace='/game')
 def left(message):
@@ -18,13 +18,13 @@ def left(message):
     leave_room(room)
     emit('status', {'msg': session.get('name') + ' has left the room.'}, room=room)
     routes.room_count[room] -= 1
+    print(routes.room_count[room])
     routes.room_members[room].remove(session.get('name'))
 
 
 @socketio.on('startgame', namespace='/game')
 def startgame():
-	#TO DO:
-	#make sure four people are in the room before 
+	#TODO: make sure four people are in the room before 
 	room = session.get('room')
 	emit('start_game', {'msg': " "}, room=room)
 	hands = bigtwo.deal(bigtwo.create_deck())
@@ -33,3 +33,9 @@ def startgame():
 		json_dictionary[routes.room_members[room][i]] = hands[i]
 	json_object = json.dumps(json_dictionary)
 	emit('deal', json_object, room=room)
+
+@socketio.on('changeupcard', namespace='/game')
+def changeupcard(json):
+	#TODO: update session variable holding the upcard, update turn variable
+	room = session.get('room')
+	emit('change_upcard', json, room=room)
